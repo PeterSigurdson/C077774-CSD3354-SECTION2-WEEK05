bffddab5bc4911642479f34cc3082985bef0660c
peter sigurdson <peter@petersigurdson.net>
2019-02-06 09:09:07 -0500

    starter code for Test Question

---
+++ C077774/Peter111.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace C077774
+{
+    class Peter111
+    {
+        static void Main(string[] args)
+        {
+            MethodA();
+        }
+
+        public static void MethodA()
+        {
+            while (Peanut())
+            {
+                Console.WriteLine("Help ! I am stuck in an infinite loop!");
+            }
+        }
+
+        public static bool Peanut()
+        {
+            return true;
+        }
+    }
+}
