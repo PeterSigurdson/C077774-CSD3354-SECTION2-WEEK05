a825dd8d0b6ef76ec32e85cc2ab3bc7ae3d8e383
peter sigurdson <peter@petersigurdson.net>
2019-02-06 09:39:10 -0500

    9:38 AM COMMIT

--- C077774/Peter111.cs
+++ C077774/Peter111.cs
@@ -4,21 +4,28 @@ namespace C077774
 {
     class Peter111
     {
+        static int i = 0;
+
         static void Main(string[] args)
-        {
-            MethodA();
+        {   MethodA();
+            Console.WriteLine("Now I am done!!!");
         }
 
         public static void MethodA()
         {
             while (Peanut())
             {
-                Console.WriteLine("Help ! I am stuck in an infinite loop!");
+                Console.WriteLine(" i is {0} ", i);
+                if (i> 10)
+                    {
+                        return;
+                    }
             }
         }
 
         public static bool Peanut()
         {
+            i++;
             return true;
         }
     }
